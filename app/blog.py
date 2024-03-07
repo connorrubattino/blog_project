57cd0863bf75297c7ee2835fa47b4dc299582650
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-07 13:15:04 -0800

    view single post by ID

--- app/blog.py
+++ app/blog.py
@@ -5,7 +5,24 @@ class Blog:
         self.users = []
         self.posts = []
         self.current_user = None
-# Method to create a new user instance and add to the Blog's user list
+
+    # Private method that will get a post by its ID or return None if no post with that ID
+    def __get_post_from_id(self):
+        # Ask the user for the ID of the post they would like to view
+        post_id = input('What is the ID of the post that you would like to get? ')
+        # Ensure that we can convert our post_id to an integer
+        while not post_id.isdigit():
+            post_id = input('Invalid ID. Must be an integer. Please enter ID again: ')
+        # Loop through all of the posts in the blog
+        for post in self.posts:
+            # If the post's ID matches the post_id argument
+            if post.id == int(post_id):
+                # Return that post instance
+                return post
+        # If we finish the loop, that means we did not find a post with that ID
+        print(f"Post with an ID of {post_id} does not exist") # 404 Not Found
+
+    # Method to create a new user instance and add to the Blog's user list
     def create_new_user(self):
         # Get user info from input
         username = input('Please enter a username: ')
@@ -21,55 +38,62 @@ class Blog:
         self.users.append(new_user)
         print(f"{new_user} has been created.")
 
-
-    # Method to log a user in by setting the current_user to a User instance
+    # Method to log a user in
     def log_user_in(self):
-        # Get the user credentials via input
+        # Ask the user for credentials
         username_input = input('What is your username? ')
         password_input = input('What is your password? ')
-        # Loop through the users in blog's user list
+        # Loop through the users in the blog's user list
         for user in self.users:
-            # Check if that user's username and password match the inputs
+            # Check if the user's username matches the username_input - same with password
             if user.username == username_input and user.check_password(password_input):
                 # If both are True, set the blog's current user to that user
                 self.current_user = user
-                print(f'{user} has logged in.')
-                # Once we find the right user, we don't need to search any more
+                print(f"{user} has logged in.")
+                # Once we find the right user, we don't know check any other users
                 break
-        # if we go through the for loop without breaking, we know we have invalid credentials
+        # if we go through the loop without breaking
         else:
+            # Then the user has bad credentials
             print('Username and/or password is incorrect.')
 
-    
-    # Method to log a user out by setting the current_user to None
+    # Method to log a user out
     def log_user_out(self):
-        username = self.current_user.username
+        print(f"{self.current_user} has been logged out.")
         self.current_user = None
-        print(f'{username} has successfully logged out')
 
     # Method to add a new post to the blog, authored by the logged in user
     def create_new_post(self):
-        # Check to make sure we have a logged in user
+        # Check to make sure that we have a lgged in user
         if self.current_user is None:
-            print("You must be logged in to perform this action") # 401 Unauthorized Status Code
+            print('You must be logged in to perform this action') # 401 Unauthorized
         else:
-            # Get the title and body for the new post from input
-            title = input("Enter new post title: ")
-            body = input("Enter new post body: ")
+            # Get the title and post from the user input
+            title = input('Enter new post title: ')
+            body = input('Enter new post body: ')
             # Create a new instance of Post with the inputted info + logged in user
             new_post = Post(title, body, self.current_user)
-            # Add the new post to the blog's posts list
+            # Add the new post to the blog's list of posts
             self.posts.append(new_post)
             print(f"{new_post.title} has been created!")
 
-
-    # Method to view all posts
+    # Method to view all of the posts in the blog
     def view_posts(self):
         # Check if there are any posts
         if self.posts:
-            # Loop through the blog's posts list
+            # Loop through the posts
             for post in self.posts:
-                # print the post (__str__ already formats it for us)
+                # Print the post (the __str__ method will format it for us)
                 print(post)
+        # If no posts
         else:
-            print("There are currently no posts for this blog :( ")
+            print('There are currently no posts in this blog :( ')
+
+    # Method to view a single post by ID
+    def view_post(self):
+        # Get the post using the private method
+        post = self.__get_post_from_id()
+        # Check if the post exists
+        if post:
+            # If so, print that post
+            print(post)
--- run.py
+++ run.py
@@ -1,37 +1,52 @@
 from app import Blog
+from app.models import User, Post
 
 
 def run_blog():
-    print("Welcome to the Kekambas Blog!")
-    # Create an instance of the Blog class
+    print('Welcome to the Kekambas Blog!')
+    # Create an instance of the blog class
     blog = Blog()
-    # Start "running" our blog until the user quits
+
+    # CREATE SOME INITIAL DATA
+    user1 = User('brians', 'abc123')
+    user2 = User('jumpman23', '6rings')
+    blog.users.append(user1)
+    blog.users.append(user2)
+    post1 = Post('Fri-yay!', 'It is Friday, hooray!', user1)
+    post2 = Post('Weekend', 'I am ready for the weekend', user2)
+    blog.posts.append(post1)
+    blog.posts.append(post2)
+
+
+    # Start "running" our blog until user quits
     while True:
         if blog.current_user is None:
-            # print our menu options
+            # Print the logged out menu option
             print("1. Sign Up\n2. Log In\n3. View All Posts\n4. View Single Post\n5. Quit")
             # Ask the user what they would like to do
             to_do = input('Which option would you like to do? ')
-            # While the user inputs an invalid option
+            # Make sure they give a valid option
             while to_do not in {'1', '2', '3', '4', '5'}:
-                # redefine to_do with a new input
-                to_do = input('Invalid option. Please choose 1, 2, 3, 4, or 5: ')
-            # Different "Routes" - different options for what to do
+                to_do = input('Invalid option. Please enter 1, 2, 3, 4, or 5: ')
+            # Different "routes" - different options to take
             if to_do == '5':
                 break
             elif to_do == '1':
-                # Call the create new user method on the blog
+                # Call the create_new_user method on the blog
                 blog.create_new_user()
             elif to_do == '2':
-                # Call the log user in function
+                # Call the log_user_in method on the blog
                 blog.log_user_in()
             elif to_do == '3':
-                #call the view_post method on the blog
+                # Call the view_posts method on the blog
                 blog.view_posts()
+            elif to_do == '4':
+                # Call the view_post method with the int version of post_id
+                blog.view_post()
             else:
-                print(f'Option {to_do} is a work in progress!')
+                print(f"Option {to_do} is coming soon!")
         else:
-            # print menu options for a logged in user
+            # Print the menu for logged in users
             print("1. Sign Out\n2. Create A Post\n3. View All Posts\n4. View Single Post\n5. Edit A Post\n6. Delete A Post")
             to_do = input('Which option would you like to do? ')
             # Make sure they give a valid option
@@ -41,19 +56,20 @@ def run_blog():
                 # Call the log_user_out method on the blog
                 blog.log_user_out()
             elif to_do == '2':
-                #call create post methon on the blogs 
+                # Call the create_post method on the blog
                 blog.create_new_post()
             elif to_do == '3':
-                #call the view_post method on the blog
+                # Call the view_posts method on the blog
                 blog.view_posts()
+            elif to_do == '4':
+                blog.view_post()
             else:
-                print(f'Option {to_do} is a work in progress!')
+                print(f"Option {to_do} is coming soon!")
 
-    
-    # Once the user quits
-    print('Thanks for checking out the blog.')
+    # Once the user quits and the while loop breaks
+    print("Thank you for checking out the blog")
     print(blog.users)
     print(blog.posts)
-    print('Goodbye!')
+
 
 run_blog()
