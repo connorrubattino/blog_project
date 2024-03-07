a2458eafbe05a23f00aadd31a63db70c18c01e0a
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-07 11:54:33 -0800

    login/logout functions and menu options

--- app/blog.py
+++ app/blog.py
@@ -4,7 +4,7 @@ class Blog:
     def __init__(self):
         self.users = []
         self.posts = []
-
+        self.current_user = None
 # Method that will take in a password guess and return True if it matches the "private" password, else False 
     def create_new_user(self):
         # Get user info from input
@@ -19,3 +19,29 @@ class Blog:
             # Add the new user to the blog's user list
             self.users.append(new_user)
             print(f"{new_user} has been created.")
+
+
+    # Method to log a user in by setting the current_user to a User instance
+    def log_user_in(self):
+        # Get the user credentials via input
+        username_input = input('What is your username? ')
+        password_input = input('What is your password? ')
+        # Loop through the users in blog's user list
+        for user in self.users:
+            # Check if that user's username and password match the inputs
+            if user.username == username_input and user.check_password(password_input):
+                # If both are True, set the blog's current user to that user
+                self.current_user = user
+                print(f'{user} has logged in.')
+                # Once we find the right user, we don't need to search any more
+                break
+        # if we go through the for loop without breaking, we know we have invalid credentials
+        else:
+            print('Username and/or password is incorrect.')
+
+    
+    # Method to log a user out by setting the current_user to None
+    def log_user_out(self):
+        username = self.current_user.username
+        self.current_user = None
+        print(f'{username} has successfully logged out')
--- run.py
+++ run.py
@@ -7,22 +7,33 @@ def run_blog():
     blog = Blog()
     # Start "running" our blog until the user quits
     while True:
-        # print our menu options
-        print("1. Sign Up\n2. Log In\n3. View All Posts\n4. View Single Post\n5. Quit")
-        # Ask the user what they would like to do
-        to_do = input('Which option would you like to do? ')
-        # While the user inputs an invalid option
-        while to_do not in {'1', '2', '3', '4', '5'}:
-            # redefine to_do with a new input
-            to_do = input('Invalid option. Please choose 1, 2, 3, 4, or 5: ')
-        # Different "Routes" - different options for what to do
-        if to_do == '5':
-            break
-        elif to_do == '1':
-            # Call the create new user method on the blog
-            blog.create_new_user()
+        if blog.current_user is None:
+            # print our menu options
+            print("1. Sign Up\n2. Log In\n3. View All Posts\n4. View Single Post\n5. Quit")
+            # Ask the user what they would like to do
+            to_do = input('Which option would you like to do? ')
+            # While the user inputs an invalid option
+            while to_do not in {'1', '2', '3', '4', '5'}:
+                # redefine to_do with a new input
+                to_do = input('Invalid option. Please choose 1, 2, 3, 4, or 5: ')
+            # Different "Routes" - different options for what to do
+            if to_do == '5':
+                break
+            elif to_do == '1':
+                # Call the create new user method on the blog
+                blog.create_new_user()
+            elif to_do == '2':
+                # Call the log user in function
+                blog.log_user_in()
+            else:
+                print(f'Option {to_do} is a work in progress!')
         else:
-            print(f'Option {to_do} is a work in progress!')
+            # print menu options for a logged in user
+            print('1. Sign Out')
+            to_do = input('Which option would you like to do? ')
+            if to_do == '1':
+                blog.log_user_out()
+
     
     # Once the user quits
     print('Thanks for checking out the blog.')
